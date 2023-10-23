aba9d17e222dd72312898d97fecc7a9979093248
Joe Rabinoff <rabinoff@post.harvard.edu>
2023-10-23 12:27:04 -0400

    Setup to hack on qtile

--- libqtile/core/lifecycle.py
+++ libqtile/core/lifecycle.py
@@ -24,7 +24,7 @@ class LifeCycle:
 
     def _atexit(self) -> None:
         if self.behavior is Behavior.RESTART:
-            argv = [sys.executable] + sys.argv
+            argv = sys.argv
             if "--no-spawn" not in argv:
                 argv.append("--no-spawn")
             argv = [s for s in argv if not s.startswith("--with-state")]
@@ -32,7 +32,7 @@ class LifeCycle:
                 argv.append("--with-state=" + self.state_file)
             logger.warning("Restarting Qtile with os.execv(...)")
             # No other code will execute after the following line does
-            os.execv(sys.executable, argv)
+            os.execv(sys.argv[0], argv)
         elif self.behavior is Behavior.TERMINATE:
             logger.warning("Qtile will now terminate")
         elif self.behavior is Behavior.NONE:
