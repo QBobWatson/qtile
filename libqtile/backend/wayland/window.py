18479823a82e0f96cd928584df50863c325b6a03
Joe Rabinoff <rabinoff@post.harvard.edu>
2023-10-25 13:45:33 -0400

    Fix static positioning in Wayland.
    The static was being positioned as if it still had a border.

--- libqtile/backend/wayland/window.py
+++ libqtile/backend/wayland/window.py
@@ -755,6 +755,8 @@ class Window(typing.Generic[S], _Base, base.Window, HasListeners):
         while self._borders:
             for rect in self._borders.pop():
                 rect.node.destroy()
+        if self.tree:
+            self.tree.node.set_position(0, 0)
 
         win = self._to_static(conf_x, conf_y, conf_width, conf_height)
 
